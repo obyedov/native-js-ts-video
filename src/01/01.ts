dcb09cd610b70a5ac428d888dfb39442b051eaf8
Vitaliy Obyedov <obyedov@gmail.com>
2022-08-28 23:20:58 +0200

    01 - tests

--- src/01/01.ts
+++ src/01/01.ts
@@ -1,2 +1,23 @@
 export const value01_01 = '01_01'
-console.log('111')
+
+export function splitIntoWords(sentense: string) {
+    let a = sentense
+        .toLowerCase()
+        .split(" ")
+        .filter(w => w !== "" && w !== "-")
+        .map(w => w
+            .replace('!', '')
+            .replace('.', '')
+            .replace(',', '')
+        )
+
+
+    return a
+}
+
+export function sum(a: number, b: number) {
+    return a + b
+}
+export function mult(a: number, b: number) {
+    return a * b
+}
